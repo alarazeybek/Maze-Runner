52a868e34492256ad3ce618caf2ff9264e996a1a
=Alara <=alara.zeybek@ug.bilkent.edu.tr>
2022-09-22 18:28:13 +0300

    CS 102 Maze Runner

---
+++ Maze Runner/MazeRunner.java
@@ -0,0 +1,20 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class MazeRunner {
+  
+  
+  public static void main(String[] args) { 
+    Maze labyrinth = new Maze();
+    System.out.println(labyrinth);
+    if(labyrinth.traverse(0,0))
+      System.out.println("Solved!");
+    else
+      System.out.println("No solution!");
+    System.out.println(labyrinth);    
+  }
+  
+  /* ADD YOUR CODE HERE */
+ 
+  
+}
